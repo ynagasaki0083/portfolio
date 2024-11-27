9d983bd393f5a3b1dcee26391b663bf13cf92495
ynagasaki0083 <j13321yn.tuis@gmail.com>
2024-11-27 14:18:11 +0900

    second commit

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,5 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: `export`,
-  images: {
-    unoptimized: true,
-  },
 };
 export default nextConfig;
